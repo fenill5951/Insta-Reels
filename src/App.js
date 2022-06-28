25d805f431918e33ac6d73725cd00ea091d9ff28
fenill5951 <fenil5951@gmail.com>
2022-06-28 08:15:59 +0530

    Insta reels commited

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './Components/signup'
+import Login from './Components/login';
+import Feed from './Components/feed';
+import {BrowserRouter,Switch,Route, Routes} from 'react-router-dom'
+import {AuthProvider,AuthContext} from './Context/AuthContext';
+import PrivateRoute from './Components/PrivateRoute';
+import {useContext} from 'react';
+import Profile from './Components/Profile';
+// function PrivateRoute({ children }) {
+//   const {user} = useContext(AuthContext);
+//   return auth ? children : <Navigate to="/login" />;
+// }
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <AuthProvider>
+    <Routes>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/login" element={<Login/>} />
+    <Route path="/profile/:id" element={< Profile/>}/>
+    <Route path="/" element={<PrivateRoute><Feed/></PrivateRoute>}/>
+    {/* <Route path="/" element={<Feed/>}/> */}
+    </Routes>
+    </AuthProvider>
+    </BrowserRouter> 
+  );
+}
+
+export default App;
